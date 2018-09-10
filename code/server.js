e035e25cb91902b24f2e1dfefb3fa958e8c33c33
JLasc <lascaze.j@gmail.com>
2018-09-10 19:47:08 -0400

    server update

--- code/server.js
+++ code/server.js
@@ -30,8 +30,8 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
+app.get("/viewtable", function(req, res) {
+    res.sendFile(path.join(__dirname, "viewtable.html"));
   });
 
 // Displays all tables
